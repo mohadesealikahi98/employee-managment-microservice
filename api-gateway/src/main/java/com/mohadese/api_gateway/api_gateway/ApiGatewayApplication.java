aa8353e5fbd8f6fb8a1865ecf16a8ff379bc6275
mohadesealikahi98 <mohadese.alikahi98@gmail.com>
2025-06-13 18:47:11 +0330

    add api-gateway

--- api-gateway/src/main/java/com/mohadese/api_gateway/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/mohadese/api_gateway/api_gateway/ApiGatewayApplication.java
@@ -1,14 +1,13 @@
-package com.mohadese.dsm;
+package com.mohadese.api_gateway.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
-public class DepartmentServiceApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DepartmentServiceApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
--- esm/src/main/java/com/mohadese/esm/EmployeeServiceApplication.java
+++ esm/src/main/java/com/mohadese/esm/EmployeeServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+
 public class EmployeeServiceApplication {
 
 //	@Bean
