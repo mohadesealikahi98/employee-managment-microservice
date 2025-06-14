f3e92ce26d1d216b8af75be2af1ca67dfe51cdf6
mohadesealikahi98 <mohadese.alikahi98@gmail.com>
2025-06-14 20:49:07 +0330

    add config server

---
+++ esm/src/main/java/com/mohadese/esm/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.mohadese.esm.controller;
+
+import com.mohadese.esm.dto.APIResponseDto;
+import com.mohadese.esm.dto.EmployeeDto;
+import com.mohadese.esm.service.EmployeeService;
+import lombok.AllArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/employees")
+@AllArgsConstructor
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    // Build Save Employee REST API
+    @PostMapping
+    public ResponseEntity<EmployeeDto> saveEmployee(@RequestBody EmployeeDto employeeDto){
+        EmployeeDto savedEmployee = employeeService.saveEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+    // Build Get Employee REST API
+    @GetMapping("{id}")
+    public ResponseEntity<APIResponseDto> getEmployee(@PathVariable("id") Long employeeId){
+        APIResponseDto apiResponseDto = employeeService.getEmployeeById(employeeId);
+        return new ResponseEntity<>(apiResponseDto, HttpStatus.OK);
+    }
+}
